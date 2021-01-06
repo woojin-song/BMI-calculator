70a32a6e4241f47e512a3b82dbee1a820ae9a94e
woojin-song <42886149+woojin-song@users.noreply.github.com>
2021-01-06 21:51:51 +0900

    new

---
+++ BMI calculator.c
@@ -0,0 +1,12 @@
+﻿#include <stdio.h>
+int main() {
+	float height, weight;
+	float BMI;
+	printf("신장 입력 (m) : ");
+	scanf("%f", &height);
+	printf("몸무게 입력 (kg) : ");
+	scanf("%f", &weight);
+	BMI = weight / (height * height);
+	printf("당신의 BMI지수는 %.3f입니다\n.", BMI);
+	return 0;
+}
